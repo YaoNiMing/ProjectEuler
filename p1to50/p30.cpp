5439ed140dc027bf20a0e9a2425e8993521cfb4a
Leo Guo <leoguo@dhcp-10-105-74-170.wireless.northwestern.private>
2016-11-21 13:27:41 -0500

    modified p30

--- p1to50/p30.cpp
+++ p1to50/p30.cpp
@@ -1,1 +1,24 @@
-hello
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	int total = 0;
+	int dig_sum,n;
+	const int max = 6*pow(9,5);
+	for (int i=10;i<max;++i)
+	{
+		dig_sum = 0;
+		n = i;
+		while (n!=0)
+		{
+			dig_sum += pow((n%10),5);
+			n /= 10;
+		}
+		if (dig_sum == i) {total+= i;
+		cout << i << endl;}
+	}
+	cout << total << endl;
+	return 0;
+}
